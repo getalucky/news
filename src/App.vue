c486fb68f0906af73f7e3f7b230814d3489f6eef
Liaotyr <1318931707@qq.com>
2020-03-24 22:05:45 +0800

    完成登陆页面的样式

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+ul {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+i {
+  text-decoration: none;
+}
+img {
+  vertical-align: bottom;
+}
+input,
+button {
+  border: 0;
+}
+input:focus,
+button:focus {
+  outline: none;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+// 适配屏幕大小：320 ———— 799
+@media screen and(min-width: 320px) and(max-width: 359px) {
+  html {
+    font-size: 12.8px;
+  }
+}
+@media screen and(min-width: 360px) and(max-width: 374px) {
+  html {
+    font-size: 14.4px;
+  }
+}
+@media screen and(min-width: 375px) and(max-width: 385px) {
+  html {
+    font-size: 15px;
+  }
+}
+@media screen and(min-width: 386px) and(max-width: 392px) {
+  html {
+    font-size: 15.44px;
+  }
+}
+@media screen and(min-width: 393px) and(max-width: 400px) {
+  html {
+    font-size: 16px;
+  }
+}
+@media screen and(min-width: 401px) and(max-width: 414px) {
+  html {
+    font-size: 16.48px;
+  }
+}
+@media screen and(min-width: 750px) and(max-width: 799px) {
+  html {
+    font-size: 30.72px;
   }
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="w">
+    <i class="logo iconfont iconnew"></i>
+    <div class="login">
+      <input type="text" placeholder="用户名 / 手机号码" />
+      <input type="password" placeholder="密码" />
+      <button>登录</button>
+    </div>
+    <i class="iconfont iconicon-test frok"></i>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang='less'>
+@import url("//at.alicdn.com/t/font_1426139_jm7n2373nlh.css");
+.w {
+  width: 22.5rem;
+  margin: auto;
+  // background: blue;
+}
+body {
+  background: #f2f2f2;
+}
+.logo {
+  display: flex;
+  justify-content: center;
+  margin-top: 3.75rem;
+  font-size: 6rem;
+  color: #d81e06;
+}
+
+.login {
+  display: flex;
+  // margin-top: 5.866667rem;
+  flex-direction: column;
+  margin-top: 3.125rem;
+  align-items: center;
+  button {
+    margin-top: 2.1875rem;
+    width: 20rem;
+    height: 3rem;
+    border-radius: 1.5rem;
+    background: #cc3300;
+    font-size: 0.533333rem;
+    color: #fff;
+  }
+  input {
+    background: inherit;
+    width: 20rem;
+    height: 3rem;
+    font-size: 1.25rem;
+    border-bottom: 1px solid #333;
+  }
+}
+.frok {
+  position: fixed;
+  top: 1.5rem;
+  left: 1.5rem;
+  font-size: 0.9375rem;
+}
+</style>
